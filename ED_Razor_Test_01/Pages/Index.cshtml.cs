00630efc52356fdad42d35efcb65bdc654395c18
Ralf Hintz <ralf.hintz@jonkoping.se>
2022-09-09 10:07:28 +0200

    Add project files.

---
+++ ED_Razor_Test_01/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ED_Razor_Test_01.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
