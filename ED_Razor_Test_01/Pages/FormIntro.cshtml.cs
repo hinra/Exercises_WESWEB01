fdea76910eb9f0e48e9a66e04821a80f76a96e6d
Ralf Hintz <ralf.hintz@jonkoping.se>
2022-09-09 11:31:55 +0200

    med forms

---
+++ ED_Razor_Test_01/Pages/FormIntro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ED_Razor_Test_01.Pages
+{
+    public class FormIntroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
