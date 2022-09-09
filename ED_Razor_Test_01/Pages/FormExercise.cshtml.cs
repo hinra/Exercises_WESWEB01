14493915774877b54ad8a6a39ed459bd7b51b3f5
Ralf Hintz <ralf.hintz@jonkoping.se>
2022-09-09 12:42:14 +0200

    Fixad POST/GET

---
+++ ED_Razor_Test_01/Pages/FormExercise.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ED_Razor_Test_01.Pages
+{
+    public class FormExerciseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
